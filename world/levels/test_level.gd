512ed2596391086cbdb7322dc1c229ddf7a8d502
Kevin <kevin31422005@gmail.com>
2024-05-20 15:18:26 -0400

    added own level

---
+++ world/levels/test_level.gd
@@ -0,0 +1,11 @@
+# test levl script
+extends Level
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	super()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	super(delta)
