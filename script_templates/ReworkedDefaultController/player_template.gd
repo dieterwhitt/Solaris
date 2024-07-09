81aebdfc23c1e32955673b4cd5383b2fc4f035a4
dieter whittingham <dieter.whitt@gmail.com>
2024-07-08 21:53:18 -0400

    tileset fix and other small fixes before merge

--- script_templates/ReworkedDefaultController/player_template.gd
+++ script_templates/ReworkedDefaultController/player_template.gd
@@ -4,12 +4,8 @@
 extends ReworkedDefaultController
 
 func _ready():
-	# override
+	super()
 	print("creating player")
-	# activate animation tree
-	_animation_tree = $AnimationTree
-	_animation_tree.active = true
-	_state_machine = _animation_tree.get("parameters/playback")
 
 func _physics_process(delta):
 	# apply physics controller
